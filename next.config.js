a7c89d1eb9d1b77aeaa1927be8fe8f6a93795422
rat218578-cloud <rat218578@gmail.com>
2026-06-12 20:29:37 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-src 'self' https://*.evo-games.com https://*.evolution.com https://*.egcdn.com;",
+          },
+        ],
+      },
+    ];
+  },
+};
 
+module.exports = nextConfig;
