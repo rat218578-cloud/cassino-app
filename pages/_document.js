9fe4750a6158220ae22ba68df5c97a938fb6a57d
rat218578-cloud <rat218578@gmail.com>
2026-06-12 20:32:40 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document';
 
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#0a0a0a" />
+        <meta name="description" content="Cassino Ao Vivo - Football Studio e outros jogos" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
