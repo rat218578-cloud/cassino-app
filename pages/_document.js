d304f819bc238db272b91bbff80b66ba94bf6854
rat218578-cloud <rat218578@gmail.com>
2026-06-12 20:48:57 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#0a0a0a" />
-        <meta name="description" content="Cassino Ao Vivo - Football Studio e outros jogos" />
       </Head>
       <body>
         <Main />
